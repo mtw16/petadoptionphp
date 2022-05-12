737678c08da21d676a8d8b6010b9bdfcc4c6fb7b
mtw16 <mwillis93@gmail.com>
2022-05-12 14:52:23 -0400

    making changes

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
   $username   = "app";
   $password   = "test";
   $dsn        = "mysql:host=$host;dbname=pets";
+  $imageURL   = "https://pet-adoption-website.s3.amazonaws.com/";
   $options    = array(
                   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
                 );
--- init.sql
+++ init.sql
@@ -4,7 +4,6 @@ CREATE TABLE pet (
     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(30) NOT NULL,
     type VARCHAR(15) NOT NULL,
-    type VARCHAR(15) NOT NULL;
     age VARCHAR(5) NOT NULL,
     breed VARCHAR(30) NOT NULL,
     photoLocation VARCHAR(250) NOT NULL,
@@ -23,6 +22,14 @@ CREATE TABLE application (
     currentPetDetails VARCHAR(250),
     comments VARCHAR(250)
 );
-INSERT INTO pet (name, type, age, breed, size, description, status)
-VALUES ('Tomcat', 'cat', '3', 'fuzzy cat', 'very nice cat', 'available');
-INSERT INTO pet (name, type, age, breed, size, description, status) VALUES ('Meowzy', 'cat', '13', 'white cat', 'small', 'very smelly cat', 'available'); 
+INSERT INTO pet (name, type, age, breed, photoLocation, size, description, status)
+VALUES ('Tomcat', 'cat', '3', 'fuzzy cat', 'cat.jpeg' , 'small', 'very nice cat', 'available');
+INSERT INTO pet (name, type, age, breed, photoLocation, size, description, status) 
+VALUES ('Meowzy', 'cat', '13', 'white cat', 'cat2.jpeg', 'small', 'very smelly cat', 'available'); 
+INSERT INTO pet (name, type, age, breed, photoLocation, size, description, status)
+VALUES ('Baxter', 'dog', '12', 'miniature schnauzer', 'dog.jpeg' , 'small', 'very nice dog', 'available');
+INSERT INTO pet (name, type, age, breed, photoLocation, size, description, status)
+VALUES ('Suka', 'dog', '6', 'husky', 'dog2.jpeg' , 'large', 'Is a drama queen', 'available');
+
+CREATE USER 'app'@localhost IDENTIFIED BY 'test';
+GRANT ALL PRIVILEGES ON *.* TO 'app'@localhost IDENTIFIED BY 'test';
